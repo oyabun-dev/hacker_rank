81b0d8240bbb6a1134d12d287a9f6b03eec75b4e
oyabun-dev <khalifaababacarbeye@esp.sn>
2025-01-03 12:11:51 +0000

    apple and oranges success

--- apple_and_orange.js
+++ apple_and_orange.js
@@ -1,32 +1,46 @@
-const housePosition = { start: 7, end: 10 }
-const applesDistanceFromTreeArray = [2, 3, -4];
-const orangesDistanceFromTreeArray = [3, -2, -4];
-const appleTreePosition = 4;
-const orangeTreePosition = 12;
-const applesPositionArray = [];
-const orangesPositionsArray = [];
+const s = 7;
+const t = 10;
+const apples = [2, 3, -4];
+const oranges = [3, -2, -4];
+const a = 4;
+const b = 12;
+const isPositionsCorrect = (a, s, t, b) => (a < s && s < t && t < b);
+
+countApplesAndOranges(s, t, a, b, apples, oranges);
 
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
+    if (!isInRangeLarge([s, t, a, apples.length, oranges.length], 1, Math.pow(10, 5)) && !isInRangeLarge([...apples, ...oranges], Math.pow(-10, 5), Math.pow(10, 5)) && !isPositionsCorrect(a, s, t, b)) {
+        return 0;
+    }
+    const appleInHouseCount = countFruitsInHouse(isFruitInHouse(getFruitsPositionArray(apples, a), s, t));
+    const orangeInHouseCount = countFruitsInHouse(isFruitInHouse(getFruitsPositionArray(oranges, b), s, t));
+    console.log(appleInHouseCount, orangeInHouseCount);
+}
 
+function isInRangeLarge(n, inf, sup) {
+    n.forEach(number => {
+        number >= inf && number <= sup;
+    })
 }
 
 function countFruitsInHouse(isFruitsInHouseArray) {
     let count = 0;
-    isFruitInHouseArray.forEach(isFruitInHouse => {
-        count = isFruitInHouse ? count++ : count;
+    isFruitsInHouseArray.forEach(isFruitInHouse => {
+        count = isFruitInHouse ? count + 1 : count;
     });
     return count;
 }
 
-function isFruitInHouse(fruitsPositionArray, housePosition) {
+function isFruitInHouse(fruitsPositionArray, houseStartingPosition, houseEndingPosition) {
     const isFruitInHouseArray = [];
     fruitsPositionArray.forEach(fruitPosition => {
-        isFruitInHouseArray.push(fruitPosition >= housePosition.start && fruitPosition <= housePosition.end)
+        isFruitInHouseArray.push(fruitPosition >= houseStartingPosition && fruitPosition <= houseEndingPosition)
     });
     return isFruitInHouseArray;
 }
 
-function getFruitsPositionArray(fruitsPositionArray, fruitDistanceFromTreeArray, fruitTreePosition) {
+function getFruitsPositionArray(fruitDistanceFromTreeArray, fruitTreePosition) {
+    const fruitsPositionArray = []
     fruitDistanceFromTreeArray.forEach(fruitDistanceFromTree => {
         fruitsPositionArray.push(fruitLandingPosition(fruitTreePosition, fruitDistanceFromTree))
     });
@@ -36,3 +50,11 @@ function getFruitsPositionArray(fruitsPositionArray, fruitDistanceFromTreeArray,
 function fruitLandingPosition(treePosition, fruitDistanceFromTree) {
     return treePosition + fruitDistanceFromTree;
 }
+
+
+// const applesPositions = getFruitsPositionArray(apples, a);
+// const orangesPositions = getFruitsPositionArray(oranges, b);
+// const isAppleInHouse = isFruitInHouse(applesPositions, s, t);
+// const isOrangeInHouse = isFruitInHouse(orangesPositions, s, t);
+// const appleInHouseCount = countFruitsInHouse(isAppleInHouse);
+// const orangeInHouseCount = countFruitsInHouse(isOrangeInHouse);
