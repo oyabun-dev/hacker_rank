bdfae12758304af64ed5b4702ddb65f85b4224a7
oyabun-dev <khalifaababacarbeye@esp.sn>
2025-02-17 20:59:00 +0000

    kangoo done

---
+++ kangaroo.js
@@ -0,0 +1,21 @@
+const x1 = 0;
+const v1 = 2;
+const x2 = 5;
+const v2 = 3;
+if (isInRange(v1, 1, 10000) && isInRange(v2, 1, 10000) && isInRange(x1, 0, 10000) && isInRange(x2, 0, 10000) && x1 < x2) {
+    console.log(kangaroo(x1, v1, x2, v2));
+}
+
+function isInRange(n, min, max) {
+    return (n >= min && n <= max);
+}
+
+function kangaroo(x1, v1, x2, v2) {
+
+    if (v1 == v2) {
+        return x1 == x2 ? "YES" : "NO";
+    } else {
+        return (x1 - x2) % (v2 - v1) == 0 && (x1 - x2) / (v2 - v1) >= 0 ? "YES" : "NO";
+    }
+
+}
